81ac9f2b56bf5e5cc3dfa9ac4130e9c9599aaab1
Olawale Ibrahim <ibrahim.olawale@earthanalytics.no>
2022-12-11 11:24:09 +0100

    create navigation and styling

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navigation = () => {
+    return (
+        <nav className="f3 link dim black underline pa3 pointer"
+             style={{display: "flex", justifyContent: "flex-end"}}>
+            <p>Sign Out</p>
+        </nav>
+    )
+}
+
+export default Navigation;
