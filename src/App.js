1fa0bc5a6c2f1a25133a31758a160c8b9da1abbd
Olawale Ibrahim <ibrahim.olawale@earthanalytics.no>
2022-12-11 12:11:11 +0100

    logo and styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Logo from './components/Logo/Logo';
 import Navigation from './components/Navigation/Navigation';
 import './App.css';
 
@@ -7,7 +9,8 @@ class App extends Component {
     return (
       <div className="App">
         <Navigation />
-        {/* <Logo />
+         <Logo />
+        {/*
         <ImageLinkForm />
         <FaceRecognition /> */}
       </div>
